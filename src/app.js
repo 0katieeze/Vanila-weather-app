965bf8c423f8d23eb4b79e119b37030c703bf59e
kate <kdj4027@gmail.com>
2023-06-27 03:04:27 +0900

    JS + HTML + css: Implemented the unit conversion( ºC / ºF)

--- src/app.js
+++ src/app.js
@@ -31,7 +31,12 @@ function displayTemperatrue(response) {
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  // ** **
+  celsiusTemperature = response.data.main.temp;
+
+  // temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
@@ -59,7 +64,36 @@ function handleSubmit(event) {
   // console.log(cityInputElement.value);
 }
 
-search("Seoul");
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  // alert(fahrenheitTemperature);
+  let temperatureElement = document.querySelector("#temperature");
+
+  // * Remove the active class the celsius link *//
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displaycelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displaycelsiusTemperature);
+
+search("Seoul");
