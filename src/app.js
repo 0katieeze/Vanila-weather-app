feae9385276e814f6a4e51ecb3900ce503b34bcb
kate <kdj4027@gmail.com>
2023-07-04 00:31:43 +0900

    Updated JS

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ function formatDate(timestamp) {
 
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
-  let day = date.getDate();
+  let day = date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   return days[day];
